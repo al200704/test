a701298a70376c1608d34c90ee821fbe9dd9b111
al200704 <alfredomaruba2@gmail.com>
2023-09-19 23:15:15 +0800

    updated 1.php

--- 1.php
+++ 1.php
@@ -1,2 +1,13 @@
-<?php
-echo "Test Pull Push";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Web PHP</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+    // Ini adalah kode PHP
+    echo "Ini adalah pesan dari PHP.";
+    ?>
+</body>
+</html>
